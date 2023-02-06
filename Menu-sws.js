9ffcec6d35ab92e1f0ec8ab933c25a80d8d75fc7
Bee182-27 <116429662+Bee182-27@users.noreply.github.com>
2023-02-06 04:14:51 -0800

    Add files via upload

---
+++ Menu-sws.js
@@ -0,0 +1,49 @@
+const OffBtn = document.getElementById("Off");
+const OnBtn = document.getElementById('On');
+const switchContainer = document.querySelector(".switch-contianer");
+// Switch On
+
+OnBtn.onclick = () => {
+    // OffBtn.classList.remove("active");
+    //OnBtn.classList.add("active");
+
+    addRemoveActive (OffBtn, OnBtn)
+    removeStandby();
+};
+
+// Switch Off
+OffBtn.onclick = () => {
+    if (OnBtn.classList.contains("active")){
+        addRemoveActive(OnBtn, OffBtn);
+        removeStandby();
+    } else {
+        return;
+    }
+};
+
+// todo: Remove standby from the switch container
+
+function removeStandby(){
+    switchContainer.classList.remove("standby");
+    if (OffBtn.classList.remove("standby")) {
+        startStandby();
+    } else {
+        switchContainer.classList.remove("standby");
+        clearTimeout(standby);
+    }
+}
+// todo: create timeout for standby
+
+let standby;
+
+function startStandby() {
+    standby = setTimeout(() => {
+        switchContainer.classList.add("standby");
+        OffBtn.classList.remove("active");
+    }, 5000);
+}
+// todo: addRemoveActive utlilty function
+function addRemoveActive(remove, add) {
+    remove.classList.remove('active');
+    add.classList.add('active');
+}
